ceb13bbe1a775a9780b61d6e95b47d72738278c6
MLGplay1298 <miguellowergameplay@gmail.com>
2025-12-20 12:32:55 -0500

    adición de los creditos y mejoras en los temas y menu principal

--- src/script/menú_principal.gd
+++ src/script/menú_principal.gd
@@ -1,13 +1,8 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
 
@@ -16,8 +11,12 @@ func _on_start_pressed() -> void:
 
 
 func _on_start_2_pressed() -> void:
-	pass # Replace with function body.
+	OS.alert("Varios ajustes para la versión 0.0.2.alfa disponible espere una actualización", "Mensaje de Alerta")
 
 
 func _on_start_3_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/esenas/UI/créditos.tscn")
